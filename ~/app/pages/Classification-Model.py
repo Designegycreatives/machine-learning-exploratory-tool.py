2a7486b47d4b8ae66b38cb739fe67614acfab603
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-13 23:56:45 +0100

    Update Classification-Model.py

--- ~/app/pages/Classification-Model.py
+++ ~/app/pages/Classification-Model.py
@@ -38,23 +38,24 @@ try:
 except:
   pass
 
-def get_imputer(imputer):
-  if imputer == 'None':
-    return 'drop'
-  if imputer == 'Most frequent value':
-    return SimpleImputer(strategy='most_frequent', missing_values=np.nan)
-  if imputer == 'Mean':
-    return SimpleImputer(strategy='mean', missing_values=np.nan)
-  if imputer == 'Median':
-    return SimpleImputer(strategy='median', missing_values=np.nan)
 
-def get_encoder(encoder):
-  if encoder == 'None':
-    return 'drop'
-  if encoder == 'OneHotEncoder':
-    return OneHotEncoder(handle_unknown='ignore', sparse=False)
-  
-def get_scaler(scaler):
+try:
+  def get_imputer(imputer):
+    if imputer == 'None':
+      return 'drop'
+    if imputer == 'Most frequent value':
+      return SimpleImputer(strategy='most_frequent', missing_values=np.nan)
+    if imputer == 'Mean':
+      return SimpleImputer(strategy='mean', missing_values=np.nan)
+    if imputer == 'Median':
+      return SimpleImputer(strategy='median', missing_values=np.nan)
+
+  def get_encoder(encoder):
+    if encoder == 'None':
+      return 'drop'
+    if encoder == 'OneHotEncoder':
+      return OneHotEncoder(handle_unknown='ignore', sparse=False)
+  def get_scaler(scaler):
     if scaler == 'None':
         return 'passthrough'
     if scaler == 'Standard scaler':
@@ -64,21 +65,21 @@ def get_scaler(scaler):
     if scaler == 'Robust scaler':
         return RobustScaler()
     
-def get_pip_mis_num(imputer, scaler):
+  def get_pip_mis_num(imputer, scaler):
     if imputer == 'None':
         return 'drop'
     pipeline = make_pipeline(get_imputer(imputer))
     pipeline.steps.append(('scaling', get_scaler(scaler)))
     return pipeline
 
-def get_pip_mis_cat(imputer, encoder):
+  def get_pip_mis_cat(imputer, encoder):
     if imputer == 'None' or encoder == 'None':
         return 'drop'
     pipeline = make_pipeline(get_imputer(imputer))
     pipeline.steps.append(('encoding', get_encoder(encoder)))
-    return pipeline
-
-def get_ml_algorithm(algorithm):
+    return pipeline 
+ 
+  def get_ml_algorithm(algorithm):
     if algorithm == 'Logistic regression':
         return LogisticRegression()
     if algorithm == 'Support vector':
@@ -87,29 +88,32 @@ def get_ml_algorithm(algorithm):
         return KNeighborsClassifier()
     if algorithm == 'Random forest':
         return RandomForestClassifier()
-      
-target_selected = st.multiselect("Choose Column:",options=df.columns)
-cat_cols_missing = st.multiselect("Choose Column:",options=df.columns)
-num_cols_missing =  st.multiselect("Choose Column:",options=df.columns)
-cat_cols =  st.multiselect("Choose Column:",options=df.columns)
-num_cols =  st.multiselect("Choose Column:",options=df.columns)
-drop_cols =  st.multiselect("Choose Column:",options=df.columns)
+  target_selected = st.multiselect("Choose Column:",options=df.columns)
+  cat_cols_missing = st.multiselect("Choose Column:",options=df.columns)
+  num_cols_missing =  st.multiselect("Choose Column:",options=df.columns)
+  cat_cols =  st.multiselect("Choose Column:",options=df.columns)
+  num_cols =  st.multiselect("Choose Column:",options=df.columns)
+  drop_cols =  st.multiselect("Choose Column:",options=df.columns)
+        
 
-X = df.drop(columns = target_selected)
-y = df[target_selected].values.ravel()
+  X = df.drop(columns = target_selected)
+  y = df[target_selected].values.ravel()
 
-st.title('Model Selection')
+  st.title('Model Selection')
 
-classifier_list = ['Logistic regression', 'Support vector', 'K nearest neighbors', 'Random forest']
-classifier_selected = st.sidebar.selectbox('', classifier_list)
+  classifier_list = ['Logistic regression', 'Support vector', 'K nearest neighbors', 'Random forest']
+  classifier_selected = st.sidebar.selectbox('', classifier_list)
 
-pipeline = Pipeline([
+  pipeline = Pipeline([
     ('preprocessing' , preprocessing),
     ('ml', get_ml_algorithm(classifier_selected))
-])
+  ])
 
-folds = KFold(n_splits = 10, shuffle=True, random_state = 0)
-cv_score = cross_val_score(pipeline, X, y, cv=folds)
-st.subheader('Results')
-st.write('Accuracy : ', round(cv_score.mean()*100,2), '%')
-st.write('Standard deviation : ', round(cv_score.std()*100,2), '%')
+  folds = KFold(n_splits = 10, shuffle=True, random_state = 0)
+  cv_score = cross_val_score(pipeline, X, y, cv=folds)
+  st.subheader('Results')
+  st.write('Accuracy : ', round(cv_score.mean()*100,2), '%')
+  st.write('Standard deviation : ', round(cv_score.std()*100,2), '%')
+
+except:
+  pass
